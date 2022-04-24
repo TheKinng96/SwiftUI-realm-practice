fc603d180e952abca31f0721175d8e4634ed7b2d
Yap Feng Yuan <fengyuan.yap@gmail.com>
2022-04-25 07:46:12 +0900

    Initial Commit

---
+++ SwiftUI-realm practicw/SwiftUI_realm_practicwApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_realm_practicwApp.swift
+//  SwiftUI-realm practicw
+//
+//  Created by Feng Yuan Yap on 2022/04/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_realm_practicwApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
